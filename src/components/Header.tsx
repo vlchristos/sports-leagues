e82152228596d56eacbaaa1d516c73449e5b466b
Christos Vlachodimitrakos <vlchristos@gmail.com>
2025-07-11 15:06:55 +0300

    Changing title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ export default function Header() {
     <header className="border-b border-gray-200 ">
       <div className="flex justify-between items-center px-4 py-2 container mx-auto">
         <h3 className="font-bold text-[#CB0300]">Home assignment – FE</h3>
-        <a href="#" className="text-sm text-gray-400 hover:text-gray-700">
+        <a
+          href="https://github.com/vlchristos/sports-leagues"
+          target="_blank"
+          className="text-sm text-gray-400 hover:text-gray-700"
+        >
           GitHub
         </a>
       </div>
